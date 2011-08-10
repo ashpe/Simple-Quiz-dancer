8e4c8ae2d92c93fe66f1813da8e3c3094b06fc48
Ashley Pope <irashp@gmail.com>
2011-08-11 09:53:05 +1000

    got next question showing.

--- lib/simple_quiz_dancer.pm
+++ lib/simple_quiz_dancer.pm
@@ -12,4 +12,31 @@ get '/' => sub {
     template 'index', { title => $quiz->title, sections => $quiz->section_keys };
 };
 
+post '/' => sub {
+
+    #$quiz->load_sections("previously_selected_sections");
+    $quiz->start();
+    redirect 'questions';
+};
+
+get '/questions' => sub {
+    
+    my $section = $quiz->next_section();
+    my $question = $quiz->next_question();    
+
+    template 'questions', { question => $question->{question}, section => $quiz->current_section, title => $quiz->title };
+};
+
+post '/questions' => sub {
+    # store answer here
+    # $quiz->answer($args);
+    redirect 'result';
+};
+
+get '/result' => sub {
+   # return answer, correctness of answer, section, title and have a link to go to next question. 
+   my $question;
+   template 'result', { answer => $question->{answer} }; 
+};
+
 true;
