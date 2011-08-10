dad9b6672aba739186ffc6dac84f835759151f71
Ashley Pope <ashley.pope@strategicdata.com.au>
2011-08-10 17:00:35 +1000

    Added base to dancer app, reads all sections onto start screen.

--- lib/simple_quiz_dancer.pm
+++ lib/simple_quiz_dancer.pm
@@ -1,10 +1,16 @@
 package simple_quiz_dancer;
 use Dancer ':syntax';
+use Modern::Perl;
+use Simple::Quiz;
 
 our $VERSION = '0.1';
+our $quiz = Simple::Quiz->new(title => "Learning Cantonese", mode => "shuffle", filename => "questions.yaml");
 
 get '/' => sub {
-    template 'index';
+
+    $quiz->load_sections();
+
+    template 'index', { title => $quiz->title, sections => $quiz->section_keys };
 };
 
 true;
