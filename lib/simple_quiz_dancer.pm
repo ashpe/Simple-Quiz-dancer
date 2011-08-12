14d4aa211261a8707428283e3fa78ef7e84b93ac
Ashley Pope <ashley.pope@strategicdata.com.au>
2011-08-12 16:19:01 +1000

    Updated to use sessions, multiple users can now work without overriding singleton quiz var

--- lib/simple_quiz_dancer.pm
+++ lib/simple_quiz_dancer.pm
@@ -4,11 +4,6 @@ use Modern::Perl;
 use Simple::Quiz;
 
 our $VERSION = '0.1';
-our $quiz    = Simple::Quiz->new(
-    title    => "Learning Cantonese",
-    mode     => "shuffle",
-    filename => "questions.yaml"
-);
 
 get '/' => sub {
 
@@ -17,6 +12,7 @@ get '/' => sub {
         mode     => "shuffle",
         filename => "questions.yaml"
     );
+
     $tmp_quiz->load_sections();
     template 'index',
       { title => $tmp_quiz->title, sections => $tmp_quiz->section_keys };
@@ -29,15 +25,26 @@ post '/' => sub {
         $sections = [];
         push @{$sections}, params->{sections};
     }
+    
+    
+    my $quiz     = Simple::Quiz->new(
+        title    => "Learning Cantonese",
+        mode     => "shuffle",
+        filename => "questions.yaml"
+    );
+    
     $quiz->load_sections($sections);
     $quiz->start();
+    
+    session quiz => $quiz;
     redirect 'questions';
 };
 
 get '/questions' => sub {
+    my $quiz = session('quiz');
     my $section  = $quiz->next_section();
     my $question = $quiz->next_question();
-
+    
     if ( scalar @{ $quiz->completed_questions } == 0 ) {
         $section  = $quiz->next_section();
         $question = $quiz->next_question();
@@ -48,6 +55,7 @@ get '/questions' => sub {
         my $total_questions = $quiz->total_questions;
         my $title           = $quiz->title;
         $quiz->reset();
+        session quiz => $quiz;
         template 'finished',
           {
             total_correct   => $total_correct,
@@ -56,6 +64,7 @@ get '/questions' => sub {
           };
     }
     else {
+        session quiz => $quiz;
         template 'questions',
           {
             question => $question->{question},
@@ -66,20 +75,23 @@ get '/questions' => sub {
 };
 
 post '/questions' => sub {
-
+    my $quiz = session('quiz');
     # store answer here
     my $answer = params->{question};
     $quiz->answer($answer);
+    session quiz => $quiz;
     redirect 'result';
 };
 
 get '/result' => sub {
 
+    my $quiz = session('quiz');
 # return answer, correctness of answer, section, title and have a link to go to next question.
     my $question =
       $quiz->sections->{ $quiz->current_section }
       ->[ $quiz->current_question ];
     my $check_answer = $quiz->answer_question_approx( $quiz->answer );
+    session quiz => $quiz;
     template 'result',
       {
         question  => $question,
